3565abc698f311106079f7b3d106cb113ce1065e
Mikhail <314159265absurdus@gmail.com>
2018-05-30 22:33:42 +0300

    Finish about page desktop, started client page

---
+++ client.php
@@ -0,0 +1,40 @@
+<?php include 'header.php'; ?>
+
+<main class="client">
+	<section class="client__killer">
+		<div class="client__social">
+		  <a href="#0" class="icon icon--vertical"><span class="icon-facebook2"></span></a>
+		  <a href="#0" class="icon icon--vertical"><span class="icon-instagram"></span></a>
+		  <a href="#0" class="icon icon--vertical"><span class="icon-youtube"></span></a>
+		  <a href="#0" class="icon icon--vertical"><span class="icon-twitter"></span></a>
+		</div>
+		<div class="client__text-box">
+			<div class="client__text">
+				<h1 class="heading--primary">Our talent</h1>
+				<p class="heading heading--sub">Leading industry names</p>
+				<a href="" class="btn btn--secondary">About us</a>
+			</div>
+		</div>
+		<div class="client__photo-control">
+			<div class="client__photo-box">
+				<img src="" alt="" class="client__photo">
+			</div>
+			<div class="client__control-box">
+				<div class="client__control-text">
+					<p class="client__subheading">Young model 2020</p>
+					<h2 class="client__heading">Georgina Alson</h2>
+				</div>
+				<div class="client__controls">
+					<a href="#0" class="btn">
+						<span class="icon-arrow"></span>
+					</a>
+					<a href="#0" class="btn">
+						<span class="icon-arrow"></span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+
+<?php include 'footer.php'; ?>
